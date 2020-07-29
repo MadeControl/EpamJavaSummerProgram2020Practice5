bf6b48cbe7254ec25bd18497dddd31928e28498e
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-29 16:03:46 +0300

    added part3

--- src/main/java/com/epam/rd/java/basic/practice5/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part3.java
@@ -1,21 +1,66 @@
 package com.epam.rd.java.basic.practice5;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Part3 {
 
     private int counter;
-
     private int counter2;
+    private final int quantityThreads = 10;
 
     public static void main(final String[] args) {
+
+        Part3 part3 = new Part3();
+        part3.compare();
+        part3.compareSync();
 
     }
 
     public void compare() {
 
+        ExecutorService executorService = Executors.newFixedThreadPool(quantityThreads);
+
+        for(int i = 0; i < quantityThreads; i++){
+            executorService.execute(new MyThread());
+        }
+
+        executorService.shutdown();
+
+
     }
 
-    public void compareSync() {
+    public synchronized void compareSync() {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(quantityThreads);
+
+        for(int i = 0; i < quantityThreads; i++){
+            executorService.execute(new MyThread());
+        }
+
+        executorService.shutdown();
 
     }
 
+    private class MyThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            boolean result = counter == counter2;
+
+            System.out.println(result);
+
+            counter++;
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            counter2++;
+
+        }
+    }
 }
