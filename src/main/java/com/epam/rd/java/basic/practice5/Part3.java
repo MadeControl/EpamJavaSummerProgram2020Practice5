3999073190a5297921f7855f4ad4a77f29e233dc
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-08-01 14:33:50 +0300

    changed part3

--- src/main/java/com/epam/rd/java/basic/practice5/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part3.java
@@ -12,8 +12,6 @@ public class Part3 {
     private int QUANTITY_THREADS = 10;
     private static final Logger LOGGER = Logger.getLogger(Part3.class.getName());
 
-    public Part3() { }
-
     public Part3(int numberOfThreads, int numberOfIterations) {
         this.QUANTITY_THREADS = numberOfThreads;
     }
@@ -21,64 +19,21 @@ public class Part3 {
 
     public static void main(final String[] args) {
 
-        Part3 part3 = new Part3();
 
-        part3.compare();
-        part3.compareSync();
 
     }
 
     public void compare() {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(QUANTITY_THREADS);
 
-        for(int i = 0; i < QUANTITY_THREADS; i++){
-
-            executorService.submit(new MyThread());
-
-        }
-
-        executorService.shutdownNow();
 
     }
 
     public void compareSync() {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(QUANTITY_THREADS);
 
-        for(int i = 0; i < QUANTITY_THREADS; i++){
-
-            synchronized (this) {
-
-                executorService.submit(new MyThread());
-
-            }
-        }
-
-        executorService.shutdownNow();
 
     }
 
-    private class MyThread implements Runnable {
 
-        @Override
-        public void run() {
-
-            boolean result = counter == counter2;
-
-            System.out.println(result);
-
-            counter++;
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                LOGGER.log(Level.SEVERE, "InterruptedException", e);
-            }
-
-            counter2++;
-
-
-        }
-    }
 }
