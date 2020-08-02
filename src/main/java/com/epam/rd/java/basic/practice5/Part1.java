c86e66c897f22e18e8b5db62df547a1c0268439b
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-08-02 12:42:46 +0300

    Added part1

--- src/main/java/com/epam/rd/java/basic/practice5/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part1.java
@@ -3,11 +3,52 @@ package com.epam.rd.java.basic.practice5;
 
 public class Part1 {
 
+    private static final double TIME = (double) 1/2;
 
     public static void main(String[] args) {
 
+        Thread myThread1 = new MyThread1();
+        myThread1.start();
+
+        Thread myThread2 = new Thread(new MyThread2());
+        myThread2.start();
 
     }
 
+    private static class MyThread1 extends Thread {
+
+        @Override
+        public void run() {
+
+            double timePassed = 0;
+            String threadName = this.getName();
+
+            while (timePassed <= 2.0) {
+
+                System.out.println(threadName);
+                timePassed += TIME;
+
+            }
+
+        }
+    }
+
+    private static class MyThread2 implements Runnable {
+
+        @Override
+        public void run() {
+
+            double timePassed = 0;
+            String threadName = Thread.currentThread().getName();
+
+            while (timePassed <= 2.0) {
+
+                System.out.println(threadName);
+                timePassed += TIME;
+
+            }
+
+        }
+    }
 
 }
