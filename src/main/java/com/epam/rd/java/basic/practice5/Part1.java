7f70d7992a34935ee8f8fe7021aeec122c172dc7
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-08-03 14:25:57 +0300

    changed part1

--- src/main/java/com/epam/rd/java/basic/practice5/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part1.java
@@ -1,14 +1,25 @@
 package com.epam.rd.java.basic.practice5;
 
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Part1 {
 
     private static final double TIME = (double) 1/2;
+    private static final Logger LOGGER = Logger.getLogger(Part1.class.getName());
 
     public static void main(String[] args) {
 
         Thread myThread1 = new MyThread1();
         myThread1.start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(2000);
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.SEVERE, "Interrupted exception", e);
+        }
 
         Thread myThread2 = new Thread(new MyThread2());
         myThread2.start();
